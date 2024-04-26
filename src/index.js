5cfef4c4355569055e1f609c91e756c38b5d64a7
ishcenko <a_ishcenko@i.ua>
2024-04-26 12:27:56 +0300

    redux-1 DELETE

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/redux-1">
+    <BrowserRouter basename="/">
       <Provider store={store}>
         <App />
       </Provider>
